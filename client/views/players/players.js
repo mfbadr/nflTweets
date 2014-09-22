250d896cc178f4b69ec9a1fd9374a152234c42b6
Mikey Badr <mfbadr@gmail.com>
2014-09-22 12:05:48 -0500

    init PlayerCtrl

---
+++ client/views/players/players.js
@@ -0,0 +1,10 @@
+(function(){
+  'use strict';
+
+  angular.module('nflTweets')
+  .controller('PlayerCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
+    $scope.title = 'PLAYERS';
+
+  }]);
+})();
+
